994b283ecf7c5f4534aa29c02218f3f4187e4b55
Brittany Walker <breneewalker@gmail.com>
2017-10-06 11:25:28 -0400

    add placeholder HTML to App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,31 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import Log from './Log';
 
 class App extends Component {
 
   render() {
 
+
     return (
-      <div className="App">
-      Hello
+      <div>
+        <header>
+          <h1>100 Days of Code | Round 2 Log</h1>
+          <p>Brittany Walker</p>
+          <p>Start Date</p>
+          <stats>
+            <ul>
+              <li>Round One Record: 195 Days</li>
+              <li>Blog Posts: 0</li>
+              <li>Tweets: 0</li>       
+              <li>Events: 0</li>   
+              <li>Presentations: 0</li>  
+              <li>Projects Completed: 0</li>                                                                             
+            </ul>
+          </stats>
+        </header>
+        <Log></Log>
+        <footer></footer>
       </div>
     );
   }

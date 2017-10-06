5892b4ee1cfc5cfe199bb68faf339950cd9db559
Brittany Walker <breneewalker@gmail.com>
2017-10-06 11:26:57 -0400

    update log data schema

--- api/models/log.js
+++ api/models/log.js
@@ -5,8 +5,24 @@ var logSchema = mongoose.Schema({
       	created       : {type: Date, default: Date.now},
         title     	  : String,
         overview      : String,
-        learning      : [String],
-        projects	    : [String]
+        learning      : {
+          learningName: String,
+          learningDescription: String,
+          learningLink: String,
+        },
+        projects	    : {
+          projectName : String,
+          projectCode: String,
+          projectDemo: String,
+          projectDescription: String,
+          projectImage: String
+        },
+        events        : {
+          eventName: String,
+          eventDescription: String,
+          eventLink: String,
+          eventImage: String
+        }
 });
 
 module.exports = mongoose.model('Log', logSchema);

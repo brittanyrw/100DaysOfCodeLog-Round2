eeb079c1d335ed1d279ad20d1bcc34e6b0eb2f0d
Brittany Walker <breneewalker@gmail.com>
2017-10-08 21:59:06 -0400

    add social media icons and begin adding content

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,19 @@ import LogSummary from './LogSummary';
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts:[]
+    };
+  }
+
   render() {
 
+    const Icon = styled.i`
+    padding: 10px;
+    margin: 0 auto;
+  `;
 
     return (
       <div>
@@ -13,16 +24,16 @@ class App extends Component {
           <h1>100 Days of Code | Round 2 Log</h1>
           <p>Brittany Walker</p>
           <p>Start Date</p>
-          <stats>
-            <ul>
-              <li>Round One Record: 195 Days</li>
-              <li>Blog Posts: 0</li>
-              <li>Tweets: 0</li>       
-              <li>Events: 0</li>   
-              <li>Presentations: 0</li>  
-              <li>Projects Completed: 0</li>                                                                             
-            </ul>
-          </stats>
+          <div>
+            <p>This is a site for my logs for the 100 Days of Code Challenge (Round 2). For more information about the challenge, vist the main here. </p>
+            <div>
+              <Icon className="fa fa-link fa-2x" aria-hidden="true"></Icon>
+              <Icon className="fa fa-github fa-2x" aria-hidden="true"></Icon>
+              <Icon className="fa fa-twitter fa-2x" aria-hidden="true"></Icon>
+              <Icon className="fa fa-medium fa-2x" aria-hidden="true"></Icon>
+              <Icon className="fa fa-codepen fa-2x" aria-hidden="true"></Icon>
+            </div>
+          </div>
         </header>
         <LogSummary></LogSummary>
         <LogSummary></LogSummary>
@@ -35,3 +46,6 @@ class App extends Component {
 }
 
 export default App;
+
+
+

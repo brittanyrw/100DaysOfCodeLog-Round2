d22be6c3ec5dd74108364523d210d5d2bbcf7cc4
Brittany Walker <breneewalker@gmail.com>
2017-10-08 22:00:58 -0400

    add react router

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react'
+import { render } from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById("root") // eslint-disable-line no-undef
-);
+render((
+    <App/>
+), document.getElementById('root'));
